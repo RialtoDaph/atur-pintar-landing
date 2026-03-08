05f0f765accc265e98b8c6ab324a8b5ef1841de4
Alto Daphino <altodaphino@gmail.com>
2026-03-08 17:51:03 +0000

    File changes

--- src/pages/Debts.jsx
+++ src/pages/Debts.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { base44 } from "@/api/base44Client";
-import { Plus, Trash2, CreditCard, CheckCircle } from "lucide-react";
+import { Plus, Trash2, CreditCard, CheckCircle, Pencil } from "lucide-react";
 import AddDebtModal from "@/components/debts/AddDebtModal.jsx";
 import AddTransactionModal from "@/components/transactions/AddTransactionModal";
 import IOUSection from "@/components/splitbill/IOUSection";
