6a2c2d1730faf217c39e7675c4a8eadc07b465fe
Alto Daphino <altodaphino@gmail.com>
2026-03-08 20:02:44 +0000

    File changes

--- src/components/goals/AddGoalModal.jsx
+++ src/components/goals/AddGoalModal.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+// eslint-disable-next-line no-unused-vars
 import { X } from "lucide-react";
 import { useAppSettings } from "@/components/utils/useAppSettings";
 import { parseRupiah } from "@/components/utils/parseRupiah";
