5e12104c529530fb8b5cc70a7661aff4497b88e4
Alto Daphino <altodaphino@gmail.com>
2026-03-16 13:44:44 +0000

    File changes

--- src/components/onboarding/OnboardingQuestionnaire.jsx
+++ src/components/onboarding/OnboardingQuestionnaire.jsx
@@ -352,7 +352,7 @@ export default function OnboardingQuestionnaire({ onClose }) {
                   </div>
                 </div>
             }
-              <NavButtons onPrev={prev} onNext={next} onSkip={() => {setHasDebt(false);next();}} canNext={hasDebt === false || hasDebt && debtName && debtRemaining} />
+              <NavButtons onPrev={prev} onNext={next} canNext={hasDebt === false || hasDebt && debtName && debtRemaining} />
             </div>
           }
 
