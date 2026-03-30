2e5f6041e26fba9ca15658d8a100a70660fe1c49
Alto Daphino <altodaphino@gmail.com>
2026-03-30 01:49:00 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import LandingPage from '@/pages/LandingPage';
 import AdminSubscriptions from '@/pages/AdminSubscriptions';
 import Dashboard from '@/pages/Dashboard';
 import ProfileSettings from '@/pages/ProfileSettings';
+import Wallets from '@/pages/Wallets';
 
 const { Pages, Layout } = pagesConfig;
 
@@ -73,6 +74,7 @@ const AuthenticatedApp = () => {
       <Route path="/Subscription" element={<LayoutWrapper currentPageName="Subscription"><Subscription /></LayoutWrapper>} />
       <Route path="/AdminSubscriptions" element={<LayoutWrapper currentPageName="AdminSubscriptions"><AdminSubscriptions /></LayoutWrapper>} />
       <Route path="/ProfileSettings" element={<LayoutWrapper currentPageName="ProfileSettings"><ProfileSettings /></LayoutWrapper>} />
+      <Route path="/Wallets" element={<LayoutWrapper currentPageName="Wallets"><Wallets /></LayoutWrapper>} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
