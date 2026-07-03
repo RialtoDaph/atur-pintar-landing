f75ca46849762f62ea3a7fedaadafeb57e4a051e
Alto Daphino <altodaphino@gmail.com>
2026-07-03 23:25:45 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { AppConfigProvider } from '@/components/utils/AppConfigContext'
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import PageNotFound from './lib/PageNotFound';
 import { AuthProvider } from '@/lib/AuthContext';
 import ErrorBoundary from '@/components/utils/ErrorBoundary';
 import LandingPage from '@/pages/LandingPage';
